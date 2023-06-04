47f09c12d362c85485270a2861c57be312119e5c
dancpluz <dan08jan@gmail.com>
2023-06-04 18:44:26 -0300

    IM DONE, RESTART

--- client/app/page.js
+++ client/app/page.js
@@ -1,33 +1,75 @@
 'use client'
 import styled from 'styled-components';
 import { io } from "socket.io-client";
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
-const socket = io("http://localhost:3001");
+const URL = "http://localhost:3001";
+const socket = io(URL);
 
 export default function Home() {
   const [trackList,setTrackList] = useState([]);
-  const [playingMusic, setPlayingMusic] = useState(null);
+  const [songPlaying, setSongPlaying] = useState(true);
+  const [text, setText] = useState('vazio')
+  const [receivedChunks,setReceivedChunks] = useState([])
+  const [currentChunk, setCurrentChunk] = useState(0)
+
+  socket.on("connect", () => {
+    console.log(`Conectado com ${socket.id}`);
+    socket.emit('updateTrackList');
+    socket.emit('sendSong');
+    }
+  )
+
+  socket.on("chunksLoaded",() => {
+    setSongPlaying(true);
+    console.log('loaded')
+  })
 
   useEffect(() => {
-    socket.on("trackList",(receivedTrackList) => {
-      // Processar a lista de músicas recebida
-      setTrackList(receivedTrackList);
-    });
-  
-    return () => {
-      socket.disconnect();
+    if (songPlaying) {
+      const interval = setInterval(() => {
+        socket.emit('sendChunk');
+        console.log('Requesting Chunk')
+      },10000);
     }
-  }, [])
-  
+    return () => { clearInterval(interval) };
+  },[]);
+
+  const audioContext = new AudioContext();
+
+  socket.on("receiveChunk", (chunk) => {
+    if (chunk.byteLength == 0) {
+      socket.emit('sendChunk');
+    } else {
+    console.log(chunk)
+    decodeAudio(chunk).then((response) => {
+      setReceivedChunks(prevChunks => [...prevChunks,response]);
+    });
+    console.log(receivedChunks);
+    }
+  });
+
+  async function decodeAudio(chunk) {
+    const audioBuffer = await audioContext.decodeAudioData(chunk);
+    return audioBuffer;
+  }
+
+  function playBuffer(audioBuffer, time) {
+    const source = audioContext.createBufferSource();
+    source.buffer = audioBuffer;
+    source.connect(audioContext.destination);
+    source.start(time);
+  }
+
   return (
     <div>
       <h1>VeighRadio</h1>
       <ul>
         {trackList.map((track) => (
-          <li key={track.id} >{track.title}</li>
+          <li key={track.id} onClick={() => selectSong(track.id)}>{track.title}</li>
         ))}
       </ul>
+      <button onClick={() => playBuffer(receivedChunks[0],0)}>Play</button>
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ import express from "express";
 import http from "http";
 import { Server as IOServer } from "socket.io";
 import queue from './queue.js';
+import { createReadStream } from "fs";
+import net from 'net';
+import { createDiffieHellmanGroup } from "crypto";
 
 const PORT = 3001;
 const app = express();
@@ -12,22 +15,55 @@ const io = new IOServer(server,{
   },
 });
 
-app.get('/',(req,res) => {
-  res.send('Servidor em Execução');
+server.listen(PORT,() => {
+  console.log(`Escutando porta ${PORT}`);
 });
 
-server.listen(PORT,() => {
-  // Evento "connection" é disparado quando um cliente se conecta ao servidor Socket.IO
-  io.on("connection",(socket) => {
-  console.log("Novo cliente conectado");
+io.on("connection", (socket) => {
 
-  // Enviar a lista de músicas para o cliente
-    queue.loadTracks('tracks');
-    console.log('Lista atualizada');
-    socket.emit("trackList",queue.tracks);
+  var chunks = [];
+
+  socket.on('updateTrackList', () => {
+    // Atualizar lista
+    console.log('Lista Atualizada')
+    }
+  )
+
+  socket.on("sendSong",() => {
+    const path = "./tracks/2. VEIGH - LUXO NO MORRO.mp3";
+
+    chunks = [];
+
+    const stream = createReadStream(path,{ highWaterMark: 546000 });
+
+    stream.on("data",(chunk) => {
+      chunks.push(chunk);
+    });
+
+    stream.on("end", () => {     
+      console.log("Loaded Chunks")
+      socket.emit("chunksLoaded")
+    });
+
   });
 
-  
+  socket.on("sendChunk", () => {
+    socket.emit('receiveChunk',chunks[0])
+    console.log(`Sent chunk, chunks left ${chunks.length}`)
+    chunks.shift();
+  })
 
-  console.log(`Ouvindo porta ${PORT}`);
-});
+})
+
+function calculateChunkSize(chunkDuration) {
+  const sampleRate = 44100; // Assuming 44.1 kHz stereo audio
+  const bytesPerSample = 2; // Assuming 16-bit audio
+  const channels = 2; // Stereo
+
+  const chunkSize = Math.ceil(
+    (chunkDuration * sampleRate * bytesPerSample * channels) / 1000
+  );
+
+  return chunkSize;
+}
+
